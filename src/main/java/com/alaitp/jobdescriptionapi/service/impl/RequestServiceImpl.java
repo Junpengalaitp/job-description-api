d50263e1aa2564e8df9a4b1fefd576193425f598
Junpeng He <hejunpeng2012@hotmail.com>
2020-05-07 20:55:42 +0800

    parse api response

--- src/main/java/com/alaitp/jobdescriptionapi/service/impl/RequestServiceImpl.java
+++ src/main/java/com/alaitp/jobdescriptionapi/service/impl/RequestServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -16,6 +17,10 @@ public class RequestServiceImpl implements RequestService {
         String url = "https://remotive.io/api/remote-jobs";
         ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
         Map<String, Object> jsonMap = JsonUtil.fromJson(response.getBody());
+        List<Map<String, Object>> jobList = (List<Map<String, Object>>) jsonMap.get("jobs");
+        for (Map<String, Object> job : jobList) {
+            System.out.println(job.get("title"));
+        }
         System.out.println(jsonMap);
     }
 }
