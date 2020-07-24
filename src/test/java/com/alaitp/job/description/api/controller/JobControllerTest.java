e1802ab0f82b5508b7868c2689bb4c4b7800f967
Junpeng.He <hejunpeng2012@hotmail.com>
2020-07-24 13:50:05 +0800

    test(controller): job controller test inherit base test

--- src/test/java/com/alaitp/job/description/api/controller/JobControllerTest.java
+++ src/test/java/com/alaitp/job/description/api/controller/JobControllerTest.java
@@ -1,22 +1,18 @@
 package com.alaitp.job.description.api.controller;
 
+import com.alaitp.job.description.api.BaseJunitTest;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
 @AutoConfigureMockMvc
-class JobControllerTest {
+class JobControllerTest extends BaseJunitTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -32,6 +28,5 @@ class JobControllerTest {
                     .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_VALUE))
                     .andExpect(MockMvcResultMatchers.jsonPath("$[*].jobTitle", Matchers.hasSize(Matchers.greaterThanOrEqualTo(10))));
         }
-
     }
 }
